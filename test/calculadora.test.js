ace60a8544140ff761663c82ce4f5cde59489901
Lucas Pereira <lucas_pereiracp@outlook.com.br>
2020-03-12 22:52:42 -0300

    segunda interação

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,22 +1,43 @@
+const { toBeDeepCloseTo }
+    = require('jest-matcher-deep-close-to');
+expect.extend({ toBeDeepCloseTo });
+
 const Calculadora = require('../lib/calculadora');
+const calculadora = new Calculadora();
 
 describe('Calculadora', () => {
-    test('cliente que ganha menos de 1000', () =>{
-        let cliente = {
+    test('cliente que ganha menos de 1000', () => {
+        let props = calculadora.montarPropostas({
             nome: 'Joao da Silva',
             salario: 800.00,
             idade: 29,
             valorFinanciado: 1000.00
-        };
-        let calculadora = new Calculadora();
-        let props = calculadora.montarPropostas(cliente);
+        });
+        
+        
         expect(props.length).toBe(2);
-        expect(props[0].total).toBeCloseTo(2000.00);
-        expect(props[0].parcelas).toBe(2);
-        expect(props[0].valorParcela).toBeCloseTo(1000.00);
-        expect(props[1].total).toBeCloseTo(2000.00);
-        expect(props[1].parcelas).toBe(3);
-        expect(props[1].valorParcela).toBeCloseTo(666.666);
+        expect(props[0]).toBeDeepCloseTo({
+            total: 2000.00,
+            parcelas: 2,
+            valorParcela: 1000.00
+        }, 2);
+        expect(props[1]).toBeDeepCloseTo({
+            total: 2000.00,
+            parcelas: 3,
+            valorParcela: 666.66
+        }, 2);
+
+
+    });
+
+
+    test('cliente que ganha de 1001 a 5000', () =>{
+        let cliente = {
+            nome: 'Maria da Silva',
+            salario: 2000.00,
+            idade: 25,
+            valorFinanciado: 1000.00
+        };
         
     });
 });
