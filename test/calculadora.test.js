0cc6d3302ab0038eb69773454c3c744a36da20a4
Lucas Pereira <lucas_pereiracp@outlook.com.br>
2020-03-13 00:55:27 -0300

    segunda interacao completa

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -21,23 +21,47 @@ describe('Calculadora', () => {
             parcelas: 2,
             valorParcela: 1000.00
         }, 2);
+
         expect(props[1]).toBeDeepCloseTo({
             total: 2000.00,
             parcelas: 3,
             valorParcela: 666.66
         }, 2);
 
+ 
+
 
     });
 
 
     test('cliente que ganha de 1001 a 5000', () =>{
-        let cliente = {
+        let props = calculadora.montarPropostas2({
             nome: 'Maria da Silva',
             salario: 2000.00,
             idade: 25,
             valorFinanciado: 1000.00
-        };
+        });
+
+        expect(props.length).toBe(3);
+        expect(props[0]).toBeDeepCloseTo({
+            total: 1300.00,
+            parcelas: 2,
+            valorParcela: 650.00
+        }, 2);
+
+        expect(props[1]).toBeDeepCloseTo({
+            total: 1500.00,
+            parcelas: 4,
+            valorParcela: 375.00
+        }, 2);
+
+        expect(props[2]).toBeDeepCloseTo({
+            total: 1500.00,
+            parcelas: 10,
+            valorParcela: 150.00
+        }, 2);
+
+
         
     });
 });
